819ad466f640da359fc9bd3be033bfb8fcfaa2c8
Игорь <mulder_pz@mail.ru>
2024-02-06 20:21:16 +0300

    Lesson_1

---
+++ Lesson_1/1_chromedriver.py
@@ -0,0 +1,9 @@
+# from selenium import webdriver
+# driver = webdriver.Chrome()
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
