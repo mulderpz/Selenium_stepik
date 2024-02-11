2239075186148f8dc96cf172f5cc175405affbd1
Игорь <mulder_pz@mail.ru>
2024-02-11 13:43:08 +0300

    Lesson_5

--- Lesson_5/home_work.py
+++ Lesson_5/home_work.py
@@ -6,20 +6,31 @@ from selenium.webdriver.chrome.service import Service
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://testautomationpractice.blogspot.com/")
+#Задание 1:
+
+#Заполнить все текстовые поля данными (почистить поля перед заполнением).
+#Проверить, что данные действительно введены, используя get_attribute() и assert.
+#Страница для выполнения задания: https://demoqa.com/text-box
+
+driver.get('https://demoqa.com/text-box')
 driver.maximize_window()
 
-time.sleep(3)
-# Найти иконку Wikipedia по имени класса
-print(driver.find_element('class name', 'wikipedia-icon'))
-driver.find_element('class name', 'wikipedia-icon').click()
-time.sleep(3)
 
-# Найти поле ввода Wikipedia по id
-print(driver.find_element('id', 'Wikipedia1_wikipedia-search-input'))
+full_name = driver.find_element('xpath','//input[@placeholder = "Full Name"]')
+full_name.clear()
+# Проверяем, что в поле пусто
+assert full_name.get_attribute("value") == ""
+full_name.send_keys('Игорь')
+time.sleep(2)
 
-# Найти кнопку поиска Wikipedia по классу
-print(driver.find_element('class name', 'wikipedia-search-button'))
 
-#Найти любой другой элемент на странице по тегу
-print(driver.find_elements("tag name", "table"))
+
+
+
+#
+# # Записываем значение поля в переменную
+# email_field_value = email_field.get_attribute("value")
+#
+# # Проверяем, что в поле email содержится введенный логин
+# assert "example@yandex.ru" in email_field_value
+
