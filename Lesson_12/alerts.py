3ff5f2b875c997e6f85dc66d5c25b8685b8ef3b6
Игорь <mulder_pz@mail.ru>
2024-02-25 14:37:54 +0300

    Lesson_11

---
+++ Lesson_12/alerts.py
@@ -0,0 +1,60 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# Создание экземпляра веб-драйвера
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 10, poll_frequency=1)
+
+# Переход на веб-страницу
+driver.get("https://demoqa.com/alerts")
+
+# Клик на кнопку, которая вызывает alert
+BUTTON_1 = ("xpath", "//button[@id='alertButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_1)).click()
+
+# Ожидание появления alert и запись его в переменную для дальнейшего взаимодействия
+alert = wait.until(EC.alert_is_present())
+#
+# Переключение на alert
+driver.switch_to.alert
+time.sleep(2)
+alert.accept()
+time.sleep(2)
+
+# Клик на кнопку, которая вызывает alert
+BUTTON_3 = ("xpath", "//button[@id='confirmButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_3)).click()
+
+alert = wait.until(EC.alert_is_present())
+
+driver.switch_to.alert
+
+alert.accept() # Принимаем алерт
+
+# Клик на кнопку, которая вызывает alert
+BUTTON_3 = ("xpath", "//button[@id='confirmButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_3)).click()
+
+alert = wait.until(EC.alert_is_present())
+
+driver.switch_to.alert
+
+alert.dismiss
+
+
+# Клик на кнопку, которая вызывает alert
+BUTTON_4 = ("xpath", "//button[@id='promtButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_4)).click()
+
+alert = wait.until(EC.alert_is_present())
+
+driver.switch_to.alert
+
+print(alert.text) # Вывод текста из алерта
